1cb5bee13daf41de85fe537fc5778c691a770592
Parsajfard <Parsajfard@gmail.com>
2026-09-23 20:36:05 -0400

    Used variables to make the UFO take off and fly away!

--- Topics/Variables/ufo-on-a-dark-night/js/script.js
+++ Topics/Variables/ufo-on-a-dark-night/js/script.js
@@ -33,8 +33,22 @@ function setup() {
  * Displays a UFO
 */
 function draw() {
+    // Dawn
+    skyShade = skyShade + 1;
     // Display the sky
     background(skyShade);
+
+    // Move a UFO
+    ufo.y = ufo.y - 2;
+    ufo.x = ufo.x + 0.5; 
+
+    // Change its fill
+    ufo.fill = ufo.fill * 0.995;
+
+    //Change its shape
+    ufo.width = ufo.width / 1.005;
+    ufo.height = ufo.height / 1.005;
+    
 
     // Draw the UFO based on its properties
     push();
